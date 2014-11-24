763a5fab74575e30b99e693cbf9ace74919d8054
ichenq <ichenq@gmail.com>
2014-11-24 23:30:51 +0800

    build script

---
+++ premake4.lua
@@ -0,0 +1,62 @@
+--
+-- Premake4 build script (http://industriousone.com/premake/download)
+--
+
+solution 'lua-mysql'
+    configurations {'Debug', 'Release'}
+    --flags {'ExtraWarnings'}
+    targetdir 'bin'
+    platforms {'x64'}
+
+    configuration 'Debug'
+        defines { 'DEBUG' }
+        flags { 'Symbols' }
+
+    configuration 'Release'
+        defines { 'NDEBUG' }
+        flags { 'Symbols', 'Optimize' }
+
+    configuration 'vs*'
+        defines
+        {
+            'WIN32',
+            'WIN32_LEAN_AND_MEAN',
+            '_WIN32_WINNT=0x0600',
+            '_CRT_SECURE_NO_WARNINGS',
+            '_SCL_SECURE_NO_WARNINGS',
+            'NOMINMAX',
+        }
+
+    project 'lua-mysql'
+        language 'C'
+        kind 'SharedLib'
+        location 'build'
+        uuid 'A75AF625-DDF0-4E60-97D8-A2FDC6229AF7'
+        files
+        {
+            'src/*.h',
+            'src/*.c',
+        }
+        includedirs
+        {
+            'dep/libmysql',
+            'dep/lua/src',
+        }
+        links 'lua52'
+
+    project 'lua52'
+        language 'C'
+        kind 'SharedLib'
+        location 'build'
+        uuid 'C9A112FB-08C0-4503-9AFD-8EBAB5B3C204'
+        defines 'LUA_BUILD_AS_DLL'
+        files
+        {
+            'dep/lua/src/*.h',
+            'dep/lua/src/*.c',
+        }
+        excludes
+        {
+            'dep/lua/src/lua.c',
+            'dep/lua/src/luac.c',
+        }         
