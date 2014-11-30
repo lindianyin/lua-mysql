1cb4d9d76cca951ebe9c16b4694c6652146504fd
ichenq <ichenq@gmail.com>
2014-11-30 23:12:23 +0800

    fix gcc compilation

--- premake4.lua
+++ premake4.lua
@@ -2,6 +2,9 @@
 -- Premake4 build script (http://industriousone.com/premake/download)
 --
 
+local this_os = os.get()
+assert(this_os == 'windows' or this_os == 'linux')
+
 solution 'lua-mysql'
     configurations {'Debug', 'Release'}
     --flags {'ExtraWarnings'}
@@ -26,17 +29,13 @@ solution 'lua-mysql'
             '_SCL_SECURE_NO_WARNINGS',
             'NOMINMAX',
         }
-
-    project 'lua'
-        language 'C'
-        kind 'ConsoleApp'
-        location 'build'
-        uuid '9C08AC41-18D8-4FB9-80F2-01F603917025'
-        files
+    configuration 'gmake'
+        buildoptions '-std=c99'
+        links 
         {
-            'dep/lua/src/lua.c',
+            'm',
+            'dl',
         }
-        links 'lua52'
         
     project 'luamysql'
         language 'C'
@@ -54,18 +53,36 @@ solution 'lua-mysql'
             'dep/lua/src',
         }
         libdirs 'bin'
-        links 
-        {
-            'libmysql',
-            'lua52',
-        }
+        links 'lua5.2'
+        if os.get() == 'windows' then
+        links 'libmysql'
+        else
+        links 'mysqlclient'
+        end
 
-    project 'lua52'
+    if os.get() == 'linux' then return end
+
+    project 'lua'
+        language 'C'
+        kind 'ConsoleApp'
+        location 'build'
+        uuid '9C08AC41-18D8-4FB9-80F2-01F603917025'
+        files
+        {
+            'dep/lua/src/lua.c',
+        }
+        links 'lua52'
+        
+    project 'lua5.2'
         language 'C'
         kind 'SharedLib'
         location 'build'
         uuid 'C9A112FB-08C0-4503-9AFD-8EBAB5B3C204'
+        if os.get() == 'windows' then
         defines 'LUA_BUILD_AS_DLL'
+        else
+        defines 'LUA_USE_LINUX'
+        end
         files
         {
             'dep/lua/src/*.h',
