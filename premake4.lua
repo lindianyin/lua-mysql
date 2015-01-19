911e40498cf0c7a7b32274ead245dae38c9fb8a9
ichenq <ichenq@gmail.com>
2015-01-19 16:37:02 +0800

    Fix build script

--- premake4.lua
+++ premake4.lua
@@ -54,41 +54,10 @@ solution 'lua-mysql'
             'dep/libmysql',
             'dep/lua/src',
         }
-        libdirs 'bin'
-        links 'lua5.2'
+        
+        links 'lua5.3'
         if os.get() == 'windows' then
         links 'libmysql'
         else
         links 'mysqlclient'
         end
-
-    -- sudo apt-get install lua5.2 liblua5.2-dev
-    if os.get() == 'linux' then return end
-
-    project 'lua'
-        language 'C'
-        kind 'ConsoleApp'
-        location 'build'
-        uuid '9C08AC41-18D8-4FB9-80F2-01F603917025'
-        files
-        {
-            'dep/lua/src/lua.c',
-        }
-        links 'lua5.2'
-        
-    project 'lua5.2'
-        language 'C'
-        kind 'SharedLib'
-        location 'build'
-        uuid 'C9A112FB-08C0-4503-9AFD-8EBAB5B3C204'
-        defines 'LUA_BUILD_AS_DLL'
-        files
-        {
-            'dep/lua/src/*.h',
-            'dep/lua/src/*.c',
-        }
-        excludes
-        {
-            'dep/lua/src/lua.c',
-            'dep/lua/src/luac.c',
-        }  
